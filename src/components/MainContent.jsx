1e24298fdf5f985a80f81f6e301c19b01b5b99d8
JohnLinus <43034261+JohnLinus@users.noreply.github.com>
2019-05-12 17:17:51 +0200

    New src 12/5
    mainInnerGrid added to main

--- src/components/MainContent.jsx
+++ src/components/MainContent.jsx
@@ -1,95 +1,104 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
 import "./mainContent.css"
 
-/* lägg form i hela gridden så kommer designen att funka */
+/* lägg form i hela */
 
-const key = 'nyckeln finns i disc, klista in här'
+const key = 'Klistra in nyckeln här'
 
 class MainContent extends Component {
-  constructor() {
-    super()
-    this.state = {
-      fromValue: '',
-      toValue: '',
-      routes: [],
-      errorMsg: null,
-      apiResult: null
+    constructor() {
+        super()
+        this.state = {
+            fromValue: '',
+            toValue: '',
+            routes: [],
+            errorMsg: null,
+            apiResult: null
+        }
+        this.handleSearchSubmit = this.handleSearchSubmit.bind(this)
     }
-    this.handleSearchSubmit = this.handleSearchSubmit.bind(this)
-  }
-  
 
-  
-   handleSearchSubmit = event => {
-    event.preventDefault()
 
-    fetch(`http://free.rome2rio.com/api/1.4/json/Search?key=${key}&oName=${this.state.fromValue}&dName=${this.state.toValue}`)
-    .then(response => response.json())
-    .then(data => {
-      console.log(data)
-      let routes = data.routes
-      this.setState({
-        routes  
-      })
-      console.log(routes)
-    })
-    .catch(err =>
-      this.setState({
-        errorMsg: err
-      })
-      )
-  }
+
+    handleSearchSubmit = event => {
+        event.preventDefault()
+
+        fetch(`http://free.rome2rio.com/api/1.4/json/Search?key=${key}&oName=${this.state.fromValue}&dName=${this.state.toValue}`)
+            .then(response => response.json())
+            .then(data => {
+                console.log(data)
+                let routes = data.routes
+                this.setState({
+                    routes
+                })
+
+
+                console.log(routes)
+            })
+            .catch(err =>
+                this.setState({
+                    errorMsg: err
+                })
+            )
+    }
     render() {
-      const routes = this.state.routes.length
-      ? this.state.routes.map((route, i) => {
-          return (
-            <li key={`${i}-react-key`}>
-              <h3>{route.name}</h3>
-              <p>Distance in km: {route.distance}</p>
-              <p>
-                Traveltime in minutes: {route.totalDuration}
-              </p>
-              <p>{route.totalTransitDuration}</p>
-              {console.log(route.name)}
-            </li>
-          )
-        })
-      : null
+        const routes = this.state.routes.length
+            ? this.state.routes.map((route, i) => {
+                return (
+
+                    <section key={`${i}-react-key`}>
+
+                        <div className="resaultBox">
+                            <h3>{route.name}</h3>
+                            <p>Distance in km: {route.distance}</p>
+                            <p>Traveltime in minutes: {route.totalDuration}</p>
+                            <p>Duration {route.totalTransitDuration}</p>
+                            <p>something from array {route.indicativePrice}</p>
+                            {console.log(route.name)}
+                        </div>
+
+                    </section>
+
+                )
+            })
+            : null
         return (
 
             <main className="mainContentInnerGrid">
 
-            <main className="mainContent"> 
-            
-
-            <div className="what"> <h1>what</h1>
-        
-        <form onSubmit={this.handleSearchSubmit}>
-          <p>Wich city are you traveling from?</p>
-          <input type='text' name='departureCity' placeholder='from?' onChange={e => this.setState({ fromValue: e.target.value })}></input>
-            
-
-          <p>And where do you wanna go?</p>
-          <select onChange={e => this.setState({toValue: e.target.value})}>
-            <option value=''>Make a choice</option>
-            <option value="ostersund">Åre</option>
-            <option value="falun">Falun</option>
-            <option value="stockholm">Stockholm</option>
-          </select>
 
 
-          <button style={{ margin: '0 0 0 20px' }}>Search</button>
-        </form>
-            </div>
-            <div className="were">were</div>
-            
-            <div className="how">how
-            <h1>Routes</h1>
-            {routes}
-            </div>
-            <div className="when">when</div>
 
-            </main>
+                <div className="what">
+
+                    <h1>what</h1>
+
+                    <form onSubmit={this.handleSearchSubmit}>
+                        <p>What?</p>
+                        <input type='text' name='departureCity' placeholder='from?' onChange={e => this.setState({ fromValue: e.target.value })}></input>
+
+
+                        <p>Where?</p>
+                        <select onChange={e => this.setState({ toValue: e.target.value })}>
+                            <option placeholder='destination?'></option>
+                            <option value="ostersund">Åre</option>
+                            <option value="falun">Falun</option>
+                            <option value="stockholm">Stockholm</option>
+                        </select>
+
+
+                        <button style={{ margin: '0 0 0 20px' }}>Search</button>
+                    </form>
+                </div>
+
+
+
+
+                <div className="how"><h1>How</h1>
+                        <div>{routes}</div>
+                    </div>
+                
+
             </main>
         );
 
