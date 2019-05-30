adff6b22d1aea00b2f601e332d253297dae509de
Ronja Brandt <37421369+RonjaBrandt@users.noreply.github.com>
2019-05-30 12:05:00 +0200

    Added Bootstrapgrid to Home.

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -4,19 +4,21 @@ import Header from "../components/Header.jsx"
 import MainContent from "../components/MainContent.jsx"
 import Footer from "../components/Footer.jsx"
 
-import "./Home.css"
+//import "./Home.css"
 
 
 class Home extends Component {
     render() {
         return (
-            <div className="grid-container">
+            <section class="container">
+              
                 <Header />
                 
                 <MainContent />
                 
                 <Footer />
-            </div>
+               
+            </section>
         );
     }
 }
